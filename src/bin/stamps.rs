0e7d0695f94e88c91623c64730c894cdddde33b3
Daniel Reiter Horn <danielrh@dropbox.com>
2019-07-28 17:54:34 -0700

    keyboard handling managed

--- src/bin/stamps.rs
+++ src/bin/stamps.rs
@@ -1,5 +1,7 @@
 extern crate sdl2;
 
+use std::time;
+use std::string::String;
 use std::collections::HashMap;
 use std::env;
 use std::path::Path;
@@ -10,10 +12,33 @@ use sdl2::mouse::Cursor;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::surface::Surface;
-struct KeyRepeatEvent {
-    key_code: sdl2::keyboard::Keycode,
-    
+static DESIRED_DURATION_PER_FRAME:time::Duration = time::Duration::from_millis(4);
+
+fn process<T:sdl2::render::RenderTarget>(event: sdl2::event::Event, canvas: &mut sdl2::render::Canvas<T>, keys_down: &mut HashMap<Keycode, ()>) -> Result<bool,String>{
+    let mut key_encountered = false;
+    match event {
+        Event::Quit{..} => return Err("Exit".to_string()),
+        Event::KeyDown {keycode: Option::Some(key_code), ..} =>{
+            if let None = keys_down.insert(key_code, ()) {
+                for (key,_)in keys_down.iter() {
+                    eprintln!("Key is down {}\n", *key)
+                }
+            }
+            key_encountered = true;
+        },
+        Event::KeyUp {keycode: Option::Some(key_code), ..} =>
+        {
+            keys_down.remove(&key_code);
+        },
+        Event::MouseButtonDown {x, y, ..} => {
+            canvas.fill_rect(Rect::new(x, y, 1, 1))?;
+            canvas.present();
+        }
+        _ => {}
+    }
+    Ok(key_encountered)
 }
+
 pub fn run(png: &Path) -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
@@ -24,8 +49,7 @@ pub fn run(png: &Path) -> Result<(), String> {
       .map_err(|e| e.to_string())?;
 
     let mut canvas = window.into_canvas().software().build().map_err(|e| e.to_string())?;
-    let mut keys_down = HashMap::<sdl2::keyboard::Keycode, ()>::new();
-    sdl_context.event()?.register_custom_event::<KeyRepeatEvent>()?;
+    let mut keys_down = HashMap::<Keycode, ()>::new();
     let surface = Surface::from_file(png)
         .map_err(|err| format!("failed to load cursor image: {}", err))?;
     let cursor = Cursor::from_surface(surface, 0, 0)
@@ -37,44 +61,26 @@ pub fn run(png: &Path) -> Result<(), String> {
 
     canvas.set_draw_color(Color::RGBA(255, 255, 255, 255));
 
-    let mut events = sdl_context.event_pump()?;
 
     'mainloop: loop {
-        for event in events.wait_iter() {
-            match event {
-                Event::Quit{..} => break 'mainloop,
-                Event::KeyDown {keycode: Option::Some(key_code), ..} =>{
-                    if let None = keys_down.insert(key_code, ()) {
-                        for (key,_)in keys_down.iter() {
-                            eprintln!("Key is down {}\n", *key)
-                        }
-                        let timer = sdl_context.timer()?.add_timer(
-                            16/*ms*/,
-                            Box::new(|| {
-                                let mut opt_evt_mgr = sdl_context.event();
-                                if let Ok(evt_mgr) = opt_evt_mgr {
-                                    evt_mgr.push_custom_event(KeyRepeatEvent{key_code:key_code});
-                                    0
-                                } else {
-                                    1
-                                }
-                            }));
-                    }
-                },
-                Event::KeyUp {keycode: Option::Some(key_code), ..} =>
-                {
-                    keys_down.remove(&key_code);
-                },
-                 Event::MouseButtonDown {x, y, ..} => {
-                    canvas.fill_rect(Rect::new(x, y, 1, 1))?;
-                    canvas.present();
-                }
-                _ => {}
+        let loop_start_time = time::Instant::now();
+        let mut events = sdl_context.event_pump()?;
+        if keys_down.len() != 0 {
+            for event in events.poll_iter() {
+                process(event, &mut canvas, &mut keys_down)?; // always break
             }
-        }
+            let process_time = loop_start_time.elapsed();
+            if keys_down.len() != 0 && process_time < DESIRED_DURATION_PER_FRAME {
+                std::thread::sleep(DESIRED_DURATION_PER_FRAME - process_time);
+            }
+        } else {
+            for event in events.wait_iter() {
+                if process(event, &mut canvas, &mut keys_down)? {
+                    break;
+                }
+            }
+        };
     }
-
-    Ok(())
 }
 
 
@@ -82,10 +88,19 @@ fn main() -> Result<(), String> {
     let args: Vec<_> = env::args().collect();
 
     if args.len() < 2 {
-        println!("Usage: cargo run /path/to/image.(png|jpg)")
+        println!("Usage: cargo run /path/to/image.(png|jpg)");
+        Ok(())
     } else {
-        run(Path::new(&args[1]))?;
+        let ret = run(Path::new(&args[1]));
+        match ret {
+            Err(x) => {
+                if x == "Exit" {
+                    Ok(())
+                } else {
+                    Err(x)
+                }
+            },
+            ret => ret,
+        }
     }
-
-    Ok(())
 }
