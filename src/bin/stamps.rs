4f10f5abeac914031aa72b3aeb66612f8eac81e8
Daniel Reiter Horn <danielrh@dropbox.com>
2019-07-28 21:52:46 -0700

    control dot with both mouse and keyboard

--- src/bin/stamps.rs
+++ src/bin/stamps.rs
@@ -14,7 +14,42 @@ use sdl2::rect::Rect;
 use sdl2::surface::Surface;
 static DESIRED_DURATION_PER_FRAME:time::Duration = time::Duration::from_millis(4);
 
-fn process<T:sdl2::render::RenderTarget>(event: sdl2::event::Event, canvas: &mut sdl2::render::Canvas<T>, keys_down: &mut HashMap<Keycode, ()>) -> Result<bool,String>{
+const MOUSE_CONSTANT: i32 = 1;
+struct SceneGraph {
+    
+}
+struct SceneState{
+    scene_graph: SceneGraph,
+    mouse_x: i32,
+    mouse_y: i32,
+    cursor: Cursor,
+}
+impl SceneState {
+    fn render<T:sdl2::render::RenderTarget>(&self, canvas: &mut sdl2::render::Canvas<T>) -> Result<(),String> {
+        canvas.set_draw_color(Color::RGBA(0, 0, 0, 255));
+        canvas.clear();
+        canvas.set_draw_color(Color::RGBA(255, 255, 255, 255));
+        canvas.fill_rect(Rect::new(self.mouse_x, self.mouse_y, 1, 1))?;
+        canvas.present();
+        Ok(())
+    }
+    fn apply_keys(&mut self, keys_down: &HashMap<Keycode, ()>) {
+        if keys_down.contains_key(&Keycode::Left) {
+            self.mouse_x -= MOUSE_CONSTANT;
+        }
+        if keys_down.contains_key(&Keycode::Right) {
+            self.mouse_x += MOUSE_CONSTANT;
+        }
+        if keys_down.contains_key(&Keycode::Up) {
+            self.mouse_y -= MOUSE_CONSTANT;
+        }
+        if keys_down.contains_key(&Keycode::Down) {
+            self.mouse_y += MOUSE_CONSTANT;
+        }
+    }
+}
+
+fn process<T:sdl2::render::RenderTarget>(state: &mut SceneState, event: sdl2::event::Event, canvas: &mut sdl2::render::Canvas<T>, keys_down: &mut HashMap<Keycode, ()>) -> Result<bool,String>{
     let mut key_encountered = false;
     match event {
         Event::Quit{..} => return Err("Exit".to_string()),
@@ -25,17 +60,23 @@ fn process<T:sdl2::render::RenderTarget>(event: sdl2::event::Event, canvas: &mut
                 }
             }
             key_encountered = true;
+            state.apply_keys(&keys_down);
         },
         Event::KeyUp {keycode: Option::Some(key_code), ..} =>
         {
             keys_down.remove(&key_code);
         },
         Event::MouseButtonDown {x, y, ..} => {
-            canvas.fill_rect(Rect::new(x, y, 1, 1))?;
-            canvas.present();
+            state.mouse_x = x;
+            state.mouse_y = y;
+        }
+        Event::MouseMotion {x, y, ..} => {
+            state.mouse_x = x;
+            state.mouse_y = y;
         }
         _ => {}
     }
+    state.render(canvas)?;
     Ok(key_encountered)
 }
 
@@ -52,30 +93,30 @@ pub fn run(png: &Path) -> Result<(), String> {
     let mut keys_down = HashMap::<Keycode, ()>::new();
     let surface = Surface::from_file(png)
         .map_err(|err| format!("failed to load cursor image: {}", err))?;
-    let cursor = Cursor::from_surface(surface, 0, 0)
-        .map_err(|err| format!("failed to load cursor: {}", err))?;
-    cursor.set();
-    
-    canvas.clear();
-    canvas.present();
-
-    canvas.set_draw_color(Color::RGBA(255, 255, 255, 255));
-
-
+    let mut scene_state = SceneState {
+        scene_graph:SceneGraph{},
+        mouse_x:0,
+        mouse_y:0,
+        cursor:Cursor::from_surface(surface, 0, 0).map_err(
+            |err| format!("failed to load cursor: {}", err))?,
+    };
+    scene_state.cursor.set();
     'mainloop: loop {
         let loop_start_time = time::Instant::now();
         let mut events = sdl_context.event_pump()?;
         if keys_down.len() != 0 {
             for event in events.poll_iter() {
-                process(event, &mut canvas, &mut keys_down)?; // always break
+                process(&mut scene_state, event, &mut canvas, &mut keys_down)?; // always break
             }
             let process_time = loop_start_time.elapsed();
             if keys_down.len() != 0 && process_time < DESIRED_DURATION_PER_FRAME {
                 std::thread::sleep(DESIRED_DURATION_PER_FRAME - process_time);
+                scene_state.apply_keys(&keys_down);
+                scene_state.render(&mut canvas)?;
             }
         } else {
             for event in events.wait_iter() {
-                if process(event, &mut canvas, &mut keys_down)? {
+                if process(&mut scene_state, event, &mut canvas, &mut keys_down)? {
                     break;
                 }
             }
